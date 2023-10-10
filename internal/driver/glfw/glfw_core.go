176d109da79ef2aa062b8e19cdc4532d5037930a
Mark Gascoyne <contact@markgascoyne.co.uk>
2023-10-10 10:45:21 +0100

    debug

--- internal/driver/glfw/glfw_core.go
+++ internal/driver/glfw/glfw_core.go
@@ -15,7 +15,7 @@ import (
 func initWindowHints() {
 	fmt.Println("Initialising glfw")
 	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 2)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 
 	//glfw.WindowHint(glfw.CocoaGraphicsSwitching, glfw.True)
 }
